c1f974955e4a8b7187725f0e507847d3616cf2b6
Alex Denisov <1101.debian@gmail.com>
2012-06-12 07:18:48 +0300

    Fix Guard changes

--- lib/guard/frank.rb
+++ lib/guard/frank.rb
@@ -48,16 +48,13 @@ module Guard
     # Called on file(s) modifications that the Guard watches.
     # @param [Array<String>] paths the changes files or paths
     # @raise [:task_has_failed] when run_on_change has failed
-    def run_on_change(paths)
+    def run_on_changes(paths)
       run(paths)
     end
 
-    # Called on file(s) deletions that the Guard watches.
-    # @param [Array<String>] paths the deleted files or paths
-    # @raise [:task_has_failed] when run_on_change has failed
-    def run_on_deletion(paths)
+    def run_on_removals(paths)
+      super
     end
-
 
     private
 
